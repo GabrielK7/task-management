b98aac9ea632870ce10330eb6ce714f36eaa65a7
GabrielK <gkovacs3405@gmail.com>
2024-08-23 03:39:49 +0200

    UserServiceJpaIm added

--- src/main/java/sk/taskmanager/task_management_system/implementation/jpa/entity/UserEntity.java
+++ src/main/java/sk/taskmanager/task_management_system/implementation/jpa/entity/UserEntity.java
@@ -1,8 +1,14 @@
 package sk.taskmanager.task_management_system.implementation.jpa.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity(name="user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class UserEntity {
     @Id
     @SequenceGenerator(name = "user_id_seq", allocationSize = 1)
@@ -21,9 +27,4 @@ public class UserEntity {
         this.email = email;
     }
 
-    public UserEntity(Long id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
 }
--- src/main/java/sk/taskmanager/task_management_system/implementation/jpa/service/UserServiceJpaImpl.java
+++ src/main/java/sk/taskmanager/task_management_system/implementation/jpa/service/UserServiceJpaImpl.java
@@ -1,39 +1,70 @@
 package sk.taskmanager.task_management_system.implementation.jpa.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import sk.taskmanager.task_management_system.api.UserService;
+import sk.taskmanager.task_management_system.api.exception.BadRequestException;
+import sk.taskmanager.task_management_system.api.exception.InternalErrorException;
+import sk.taskmanager.task_management_system.api.exception.ResourceNotFoundException;
 import sk.taskmanager.task_management_system.api.request.UserAddRequest;
 import sk.taskmanager.task_management_system.domain.User;
+import sk.taskmanager.task_management_system.implementation.jpa.entity.UserEntity;
 import sk.taskmanager.task_management_system.implementation.jpa.repository.UserJpaRepository;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 @Primary
 public class UserServiceJpaImpl implements UserService {
-  private final  UserJpaRepository repository;
+    private final UserJpaRepository repository;
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceJpaImpl.class);
 
-    public UserServiceJpaImpl(UserJpaRepository jpaRepository, UserJpaRepository repository) {
+    public UserServiceJpaImpl(UserJpaRepository repository) {
         this.repository = repository;
     }
 
     @Override
     public long add(UserAddRequest request) {
-        return 0;
+        try {
+            return repository.save(new UserEntity(request.getName(), request.getEmail())).getId();
+
+        } catch (DataIntegrityViolationException e) {
+            throw new BadRequestException("User with email " + request.getEmail() + "already exists");
+        } catch (DataAccessException e) {
+            logger.error("Error while adding user", e);
+            throw new InternalErrorException("Error while adding user");
+        }
     }
 
     @Override
     public void delete(long id) {
-
+        if (this.get(id) != null) {
+            repository.deleteById(id);
+        }
     }
 
     @Override
     public User get(long id) {
-        return null;
+        // final Optional<UserEntity> userEntityOptional = repository.findById(id);
+//        if (userEntityOptional.isPresent()) {
+//            return mapUserEntitytoUser(userEntityOptional.get());
+//        } else throw new ResourceNotFoundException("User with id " + id"was not found");
+        return repository.findById(id)
+                .map(this::mapUserEntityToUser)
+                .orElseThrow(() -> new ResourceNotFoundException("User with id \" + id\"was not found"));
     }
 
     @Override
     public List<User> getAll() {
-        return List.of();
+        return repository.findAll().stream().map(this::mapUserEntityToUser).toList();
+    }
+
+    private User mapUserEntityToUser(UserEntity entity) {
+        return new User(entity.getId(), entity.getName(), entity.getEmail());
     }
 }
